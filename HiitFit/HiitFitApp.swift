eeb0cbceefb01c1f94f6334e86fc3c9da8c8983b
ennbou <ennbou@yandex.com>
2023-12-06 18:52:04 +0100

    Initial Commit

---
+++ HiitFit/HiitFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiitFitApp.swift
+//  HiitFit
+//
+//  Created by ENNBOU on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct HiitFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
