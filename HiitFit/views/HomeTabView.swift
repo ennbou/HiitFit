4b41dd6e27507de041d5ef907cbec83ef5bef3d3
ennbou <ennbou@yandex.com>
2023-12-18 16:38:57 +0100

    first commit

---
+++ HiitFit/views/HomeTabView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTabView.swift
+//  HiitFit
+//
+//  Created by ENNBOU on 12/6/23.
+//
+
+import SwiftUI
+
+struct HomeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeTabView()
+    }
+}
