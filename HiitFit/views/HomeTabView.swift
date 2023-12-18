23ad216a439e1bc18477ebbc267fccafe3e3b5bc
ennbou <ennbou@yandex.com>
2023-12-18 16:40:07 +0100

    first commit

--- HiitFit/views/HomeTabView.swift
+++ HiitFit/views/HomeTabView.swift
@@ -7,14 +7,43 @@
 
 import SwiftUI
 
-struct HomeTabView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct ViewTest: View {
+  
+  let title: String
+  let color: Color
+  
+  var body: some View {
+    return ZStack{
+      color.ignoresSafeArea()
+      Text(title)
+        .font(.title)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+    .tabItem{
+      Text("Title")
+    }
+  }
+}
+
+struct HomeTabView: View {
+  var body: some View {
+    GeometryReader{ geometry in
+      TabView{
+        NavigationStack{
+          ViewTest(title: "tab1", color: .blue)
+        }
+        ViewTest(title: "tab1", color: .yellow)
+        ViewTest(title: "tab1", color: .red)
+      }
+      .tabViewStyle(.page(indexDisplayMode: .always))
+      .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+      .ignoresSafeArea()
+    }
+  }
 }
 
 struct HomeTabView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeTabView()
-    }
+  static var previews: some View {
+    HomeTabView()
+  }
 }
